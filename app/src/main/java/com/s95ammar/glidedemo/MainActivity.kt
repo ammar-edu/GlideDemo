fbdee5b32ec12db828fd378fd50ac1788f379df0
Ammar <ammar.courses@gmail.com>
2021-05-22 20:51:20 +0300

    Init

---
+++ app/src/main/java/com/s95ammar/glidedemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.s95ammar.glidedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
